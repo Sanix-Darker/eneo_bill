3c0942cd20e5929adb76c89199339ad656d77f16
sanix-darker <s4nixd@gmail.com>
2020-03-29 13:14:59 +0100

    Updating the settings configuration and start the eneo_bill core

---
+++ settings.py
@@ -0,0 +1,12 @@
+import configparser as ConfigParser
+
+# Configs parameters configParser.get('your-config', 'path1')
+configParser = ConfigParser.RawConfigParser()
+configFilePath = r'eneo_config.txt'
+configParser.read(configFilePath)
+
+# Parameters
+USERNAME = configParser.get('eneo-config', 'USERNAME')
+PASSWORD = configParser.get('eneo-config', 'PASSWORD')
+
+LOGIN_URL = configParser.get('eneo-config', 'LOGIN_URl')

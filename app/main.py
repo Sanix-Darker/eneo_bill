8d11f60f09935eee1b53251b1a0e33ea44056071
sanix-darker <s4nixd@gmail.com>
2020-03-29 14:13:12 +0100

    Update the main core file

--- app/main.py
+++ app/main.py
@@ -10,35 +10,45 @@ print("[+] ENEO-BILL started!")
 session_requests = requests.session()
 
 # Get login _token
+print("[+] Connecting to eneocameroon")
 result = session_requests.get(LOGIN_URL)
 tree = html.fromstring(result.text)
+
+print("[+] Fetching token..")
 _token = list(set(tree.xpath("//input[@name='_token']/@value")))[0]
+
+print("[+] _token: ", _token)
 
 # Create the payload for the submission form
 payload = {
-    "user[login]": USERNAME,
-    "user[password]": PASSWORD,
+    "login": USERNAME,
+    "password": PASSWORD,
     "_token": _token
 }
 
-# Perform login
-result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
-
-
 def main():
+    print("[+] Perform the Login...")
+    # Perform login
+    result = session_requests.post(LOGIN_URL, data=payload, headers=dict(referer=LOGIN_URL))
 
+    if result.status_code != 404:
+        print("[+] Login successfully !")
+        
+    
     bill_url = "https://my.eneocameroon.cm/"
+    print("[+] Fetching bills on : ", bill_url)
     # Let's scrap the content
-    result_content = session_requests.get(bill_url, headers=dict(referer=bill_url))
+    r = session_requests.get(bill_url, headers=dict(referer=bill_url))
 
     # Let's use beautifullSoup to parse the content
-    soup = BeautifulSoup(result.content)
+    soup = BeautifulSoup(r.content, features="lxml")
     rows = soup.find("table", {"class": "table"}, border=1).find("tbody").find_all("tr")
 
+    print("[+]List of bills :")
     for row in rows:
         cells = row.find_all("td")
         the_cell = cells[0].get_text()
 
-        print("the_cell : ", the_cell)
+        print("[+] >> ", the_cell)
 
 main()
